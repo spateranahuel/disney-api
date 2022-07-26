5f6ae54e121fddec3e95c522e3479cb27fe4d470
Torinoko <spateranahuel@gmail.com>
2022-07-26 14:59:51 -0300

    Initial commit

---
+++ src/main/java/com/alkemy/disney/disney/DisneyApplication.java
@@ -0,0 +1,14 @@
+package com.alkemy.disney.disney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DisneyApplication.class, args);
+		System.out.println("HOLAAAAAAAAAAAAAAAAAAAAAA");
+	}
+
+}
